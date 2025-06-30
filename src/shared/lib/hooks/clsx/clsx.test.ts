2d9b94c2ed507dc6dbc769854fd2105bf5a66896
Maxim <schubert.walk@yandex.ru>
2025-06-30 16:29:10 +0300

    feat: add css util

---
+++ src/shared/lib/hooks/clsx/clsx.test.ts
@@ -0,0 +1,17 @@
+import { test } from 'vitest';
+
+// TODO: Написать тест кейсы, когда уже будет базовое
+// понимание как я использую clsx
+test('test clx', () => {
+    // const condition = true;
+    // const result = clsx(
+    //     sidebar.Sidebar,
+    //     !condition ? sidebar.open : sidebar.hidden,
+    //     false && sidebar.hidden,
+    //     {
+    //         [sidebar.open]: condition,
+    //         [sidebar.hidden]: !condition
+    //     },
+    //     [sidebar.disabled, sidebar.enabled]
+    // );
+});
---
+++ src/shared/lib/hooks/clsx/clsx.ts
@@ -0,0 +1,39 @@
+type ClsxParams = (boolean | string | Record<string, boolean | string> | string[])[];
+/**
+ * Объединяет классы в одну строку по аналогии с популярной библиотекой clsx.
+ *
+ * Принимает любое количество аргументов: строки, массивы строк, объекты вида {className: true/false}, булевы значения.
+ * Булевы значения и falsy-значения игнорируются.
+ *
+ * @param {...(string | boolean | Record<string, boolean | string> | string[])[]} args - Классы для объединения
+ * @returns {string} Строка с уникальными классами, разделёнными пробелом
+ *
+ * @example
+ * clsx('foo', { bar: true, baz: false }, ['qux', 'quux'], false) => 'foo bar qux quux'
+ */
+export const clsx = (...args: ClsxParams): string => {
+    const classSet = new Set<string>();
+
+    args.forEach((className) => {
+        if (!className) {
+            return;
+        }
+        if (Array.isArray(className)) {
+            for (const item of className) {
+                if (item) {
+                    classSet.add(item);
+                }
+            }
+        } else if (typeof className === 'object') {
+            for (const key in className) {
+                if (Object.prototype.hasOwnProperty.call(className, key)) {
+                    classSet.add(key);
+                }
+            }
+        } else {
+            classSet.add(String(className));
+        }
+    });
+
+    return Array.from(classSet).join(' ');
+};
